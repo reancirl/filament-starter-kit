51cd5b0148a51c4c38f57028259ff3c96080b662
Rey <reancirl@codesquirrel.com.au>
2025-01-13 08:19:19 +0800

    rever changes

--- database/migrations/2025_01_10_000000_create_users_table.php
+++ database/migrations/2025_01_10_000000_create_users_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            // $table->foreignId('instance_id')->nullable()->constrained()->cascadeOnDelete();
-            // $table->string('first_name');
-            // $table->string('middle_name')->nullable();
-            // $table->string('last_name');
-            $table->string('name');
+            $table->foreignId('instance_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
